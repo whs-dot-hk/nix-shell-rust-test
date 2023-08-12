7bf4f0bc05a1baf0cde0f0994609e5729dfb9c3b
Your Name <you@example.com>
2023-08-12 07:55:42 +0000

    First commit

--- target/debug/deps/nix_shell_rust_test-d8bc26f7236cdfb9.d
+++
@@ -1,5 +0,0 @@
-/home/packer/nix-crane-test3/target/debug/deps/nix_shell_rust_test-d8bc26f7236cdfb9: src/main.rs
-
-/home/packer/nix-crane-test3/target/debug/deps/nix_shell_rust_test-d8bc26f7236cdfb9.d: src/main.rs
-
-src/main.rs:
--- target/debug/nix-shell-rust-test.d
+++
@@ -1,1 +0,0 @@
-/home/packer/nix-crane-test3/target/debug/nix-shell-rust-test: /home/packer/nix-crane-test3/src/main.rs
